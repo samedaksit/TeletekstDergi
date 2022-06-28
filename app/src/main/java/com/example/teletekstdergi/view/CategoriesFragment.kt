7a01469166aeb789054fff29b5adef7505e3186e
aksitsamed <aksitsamed7@gmail.com>
2022-06-28 15:18:50 +0300

    first commit

---
+++ app/src/main/java/com/example/teletekstdergi/view/CategoriesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.teletekstdergi.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teletekstdergi.R
+
+
+class CategoriesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
